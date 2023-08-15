fb8eb948038eae278103b09a4379e30261dd10fc
EMILI PORTO DA SILVA <07062820199@ifms.edu.br>
2023-08-15 07:33:59 -0400

    first commit

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,34 @@
+import './footer.css'
+function Footer(){
+return(
+<footer className="rodape">
+<div>
+   
+   <h1> AppMaster</h1> 
+   <p>AppMaster é uma plataforma sem código de nova geração para automatizar processos de negócios e criar aplicativos nativos para Web e dispositivos móveis com geração de código.</p>
+</div>
+
+<footer className="produto">
+<div>
+   <h2>produtos</h2>
+   <a>Aplicativos móveis</a>
+</div>
+</footer>
+
+<footer className="rodape">
+<div>
+   <h3>Empresa</h3>
+   <a>carreiras</a>
+</div>
+</footer>
+
+<footer className="rodape"></footer>
+<div>
+<h4>recursos</h4>
+<a>agg ggfggg</a>
+</div>
+
+</footer>
+)
+}
+export default Footer
---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,19 @@
+import './Header.css'
+function Header(){
+    return(
+        <header>
+            <div>
+                logo
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="Home">Home</a></li>
+                    <li><a href="Sobre">sobre</a></li>
+                    <li><a href="Noticia"></a>Noticia</li>
+                    <li><a href="Contato"></a>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
