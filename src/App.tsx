d790c6ce95850a7320349e601aa8c662636290f5
miiilyy <emili.silva@estudante.ifms.edu.br>
2023-08-29 11:32:32 -0400

    projeto pronto

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@
 import './App.css'
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
+import Main from './componentes/main/Main'
 
 function App() {
   return (
 
-    <div>
+   <>
   <Header/>
+  <Main/>
   <Footer/>
-  </div>
+  </>
   )
 } 
 
--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,9 +1,15 @@
 import './Footer.css'
-import Logo from '../../assets/Barbie_Logo.svg.png'
 export default function Footer(){
     return(
         <footer>
-                <img src={Logo} alt=""/>
+            <div className='rodape'> 
+             < h1> Emili Portifólio</h1>
+             <a>Home</a>
+             <a>Entrem em contato</a>
+             <a>Informações</a>
+             </div>
+
         </footer>
+
     )
 }
